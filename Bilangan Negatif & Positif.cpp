b01bdcc264cdfed9ea8b3473d35e815dc6c1f43f
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 16:22:54 +0700

    Add files via upload

---
+++ Bilangan Negatif & Positif.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,MAX=0;
+
+cout<<"Program menentukan bilangan negatif dan positif"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"masukan bilangan"<<endl;
+cin>>a;
+
+if(a > MAX)
+    cout<<"Maka bilangan tersebut positif";
+    else
+        cout<<"Maka bilangan tersebut negatif";
+    return 0;
+}
